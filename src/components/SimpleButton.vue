7d90a48d182b4831aaa40941200d08b013d238d4
xavergruber <xavergruber@webundsoehne.com>
2022-07-26 16:12:21 +0200

    simple button implemented

---
+++ src/components/SimpleButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="doSomething">Click Me!</button>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const counter = ref(0)
+
+function doSomething(){
+  counter.value++;
+  console.log('something happened', counter.value)
+}
+</script>
